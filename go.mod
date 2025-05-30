ad8b290eef7f188404223ee533479604bf7b66df
motty <motty.mio2@gmail.com>
2025-05-30 23:29:23 +0900

    init

---
+++ go.mod
@@ -0,0 +1,11 @@
+module github.com/motty-mio2/incheck
+
+go 1.24.3
+
+require (
+	github.com/google/uuid v1.6.0 // indirect
+	github.com/prometheus-community/pro-bing v0.7.0 // indirect
+	golang.org/x/net v0.40.0 // indirect
+	golang.org/x/sync v0.14.0 // indirect
+	golang.org/x/sys v0.33.0 // indirect
+)
---
+++ main.go
@@ -0,0 +1,92 @@
+package main
+
+import (
+	"flag"
+	"net"
+	"os"
+
+	probing "github.com/prometheus-community/pro-bing"
+)
+
+func main() {
+	var (
+		ipaddr = flag.String("i", "", "IP address to check")
+		host   = flag.String("h", "", "Host to check")
+	)
+
+	flag.Parse()
+
+	if ping_to_host(*ipaddr) && nslookup(*host) {
+		os.Exit(0)
+	} else {
+		os.Exit(1)
+	}
+
+}
+
+// Ping the host
+func ping_to_host(host string) bool {
+	pinger, err := probing.NewPinger(host)
+	pinger.SetDoNotFragment(true)
+	if err != nil {
+		return false
+	}
+	pinger.Count = 1
+	pinger.Timeout = 3000000000
+	err = pinger.Run()
+	if err != nil {
+		return false
+	}
+
+	return true
+}
+
+// Nslookup
+func nslookup(host string) bool {
+	_, err := net.LookupHost(host)
+	if err != nil {
+		return false
+	}
+	return true
+}
+
+// Ipaddress match
+func check_ipaddr(ipaddress string) bool {
+	ip := net.ParseIP(ipaddress)
+	if ip == nil {
+		return false
+	}
+
+	mask := net.CIDRMask(24, 32)
+	networkAddr := ip.Mask(mask)
+
+	ift, err := net.Interfaces()
+	if err != nil {
+		return false
+	}
+
+	for _, ifi := range ift {
+		addrs, err := ifi.Addrs()
+		if err != nil {
+			return false
+		}
+		for _, addr := range addrs {
+			ipNet, ok := addr.(*net.IPNet)
+			if !ok {
+				continue
+			}
+			ip := ipNet.IP
+			if ip.To4() == nil {
+				continue
+			}
+
+			localNetworkAddr := ip.Mask(mask)
+
+			if networkAddr.Equal(localNetworkAddr) {
+				return true
+			}
+		}
+	}
+
+	return false
+}
